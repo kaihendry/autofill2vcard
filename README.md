552e48c82c4bf98dcdfb30869f0499a80c6a57a2
Kai Hendry <hendry@webconverger.com>
2015-09-27 19:17:09 +0800

    Don't need these stinky vcard PHP libs

---
+++ README.md
@@ -0,0 +1,3 @@
+Notice how we leverage these [autofill field
+names](https://html.spec.whatwg.org/multipage/forms.html#autofill) to make
+entering address details as simple as allowing the browser to autofill?
--- vcf/index.php
+++ vcf/index.php
@@ -1,27 +1,20 @@
 <?php
-
 header("Content-Type:text/plain");
-
-print_r($_POST);
-
-require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__ . '/vendor/jeroendesloovere/vcard/src/VCard.php';
-use JeroenDesloovere\VCard\VCard;
-
-// define vcard
-$vcard = new VCard();
-
-$vcard->addName($lastname, $firstname, $additional, $prefix, $suffix);
-
-$vcard->addCompany($_POST['organization']);
-$vcard->addEmail($_POST['email']);
-$vcard->addPhoneNumber($_POST['tel'], 'WORK');
-$vcard->addAddress(null, null, $_POST['address-line1'], $_POST['address-line2'], null, $_POST['postal-code'], $_POST['country-name']);
-$vcard->addURL($_POST['url']);
-
-// return vcard as a download
-$vcf = $vcard->getOutput();
-echo $vcf;
-echo file_put_contents("foo.vcf", $vcf);
-
 ?>
+BEGIN:VCARD
+VERSION:3.0
+REV:<?=date("c")?>
+
+FN;CHARSET=UTF-8:<?=$_POST['name']?>
+
+EMAIL;INTERNET:<?=$_POST['email']?>
+
+TEL;WORK:<?=$_POST['tel']?>
+
+ADR;WORK;POSTAL:;;<?=$_POST['street-address']?>;<?=$_POST['address-level2']?>;<?=$_POST['postal-code']?>;<?=$_POST['country-name']?>
+
+ORG:<?=$_POST['organization']?>
+
+URL:<?=$_POST['url']?>
+
+END:VCARD
